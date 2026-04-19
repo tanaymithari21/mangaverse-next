f8c597ee145997dbc85817bc51148378082dcf34
Smash-21 <tanaymithari@gmail.com>
2026-04-19 20:51:41 +0530

    .

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,18 +1,10 @@
 "use client"
 
 import { useToast } from "@/components/ui/use-toast";
-import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast";
+import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast";
 
 export function Toaster() {
   const { toasts } = useToast();
-
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
